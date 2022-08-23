ebf3cb45df1c6c2da5c31598dc05d8d25567aa21
Md. Kamrul Hasan <55704250+kamrul15-8841@users.noreply.github.com>
2022-08-23 19:10:05 +0600

    1

---
+++ script.js
@@ -0,0 +1,25 @@
+// const monthEl = document.querySelector(".date h1");
+// const monthInx = new Date();
+const monthInx = new Date().getMonth;
+// monthInx.getMonth;
+console.log(monthInx);
+// const months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+// ];
+
+// console.log(months[monthInx]);
+// monthEl.innerText = months[monthInx];
+// monthEl.innerText = monthInx;
+
+// monthEl.innerText = months [monthInx];
